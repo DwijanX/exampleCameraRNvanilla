91b6084b8c97a1c2a255910d4677fe6092f6f8fe
Gabriel Ichiro <gabriel582084@gmail.com>
2024-03-03 20:49:13 -0400

    added camera permission for ios

--- App.tsx
+++ App.tsx
@@ -1,35 +1,26 @@
 import React, {useEffect} from 'react';
-import {StyleSheet, PermissionsAndroid} from 'react-native';
+import {StyleSheet, Platform} from 'react-native';
 import {WebView} from 'react-native-webview';
+import {request, PERMISSIONS} from 'react-native-permissions';
 
 export default function App() {
-  const requestCameraPermission = async () => {
-    try {
-      if (await PermissionsAndroid.check('android.permission.CAMERA')) {
-        return;
-      }
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.CAMERA,
-        {
-          title: 'Cool Photo App Camera Permission',
-          message:
-            'Cool Photo App needs access to your camera ' +
-            'so you can take awesome pictures.',
-          buttonNegative: 'Cancel',
-          buttonPositive: 'OK',
-        },
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log('You can use the camera');
-      } else {
-        console.log('Camera permission denied');
-      }
-    } catch (err) {
-      console.warn(err);
+  const requestPermission = () => {
+    let permissionType = null;
+    if (Platform.OS === 'ios') {
+      permissionType = PERMISSIONS.IOS.CAMERA;
+    } else if (Platform.OS === 'android') {
+      permissionType = PERMISSIONS.ANDROID.CAMERA;
+    }
+
+    if (permissionType) {
+      request(permissionType).then(result => {
+        console.log(result);
+      });
     }
   };
+
   useEffect(() => {
-    requestCameraPermission();
+    requestPermission();
   }, []);
 
   return (
